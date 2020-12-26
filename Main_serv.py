c13c419a7fba1ab00157395395073e5795e9a9c5
Pavlo <kurmanp@gmail.com>
2020-12-27 01:40:18 +0200

    CheckDot_Serv_0.2

--- Main_serv.py
+++ Main_serv.py
@@ -1,55 +1,80 @@
 from socket import *
 import pyodbc as sql
+import pandas as pd
 
+cnxn_str = ("Driver={SQL Server Native Client 11.0};"
+            "Server=localhost;"
+            "Database=CheckDotBD;"
+            "Trusted_Connection=yes;")
 
-
-#connection = sql.connect("Driver={SQL Server};""Server=DESKTOP-4MN9DR5\kurma;"
-#                    "Database=CheckDotBD;""Trusted_Connection=yes", autocommit=True)
-
+cnxn = sql.connect(cnxn_str)
+cursor = cnxn.cursor()
+print(pd.read_sql("SELECT * FROM Employee", cnxn))
 s = socket()
 
 s.bind(('', 9999))
 
 s.listen(5)
-Con_num=1
-admin_st=1
+
+
+def Search(df, login, password):
+    global admin_st
+    reslog = False
+    try:
+        passcheck = df[df['login'] == login]['password'].iloc[0]
+        admin_st = int(df[df['login'] == login]['admin_st'].iloc[0])
+        if passcheck == password:
+            reslog = True
+        print(reslog, passcheck,admin_st)
+        return reslog
+    except:
+        return reslog
+
+Con_num = 1
+admin_st = 0
+datal = ''
+datap = ''
 
 while True:
     try:
         conn, addr = s.accept()
-        print(Con_num,'Connected', addr)
+        print(Con_num, 'Connected', addr)
         choise = conn.recv(1).decode()
         print('Choise', choise)
-        if choise=='l':
-            datal = ''
-            datap = ''
+        if choise == 'l':  # Login
+            data_sql = pd.read_sql("SELECT L.admin_st,L.login,P.password FROM Employee as L INNER JOIN Employee_p as P "
+                                   "on L.Employee_id =P.Employee_id ", cnxn)
             conn.send('1'.encode())
-            datal=conn.recv(1024).decode()
+            datal = conn.recv(1024).decode()
             conn.send('1'.encode())
-            datap=conn.recv(1024).decode()
-            if admin_st == 1:
-                conn.send('adm'.encode())
+            datap = conn.recv(1024).decode()
+            log_st = Search(data_sql,datal,datap)
+            print(log_st)
+            if log_st == True:
+                if admin_st == 1:
+                    conn.send('adm'.encode())
+                else:
+                    conn.send('1'.encode())
             else:
-                conn.send('1'.encode())
-            print(datal, datap)
-            choise=''
-        elif choise=='c':
+                conn.send('2'.encode())
+            print( data_sql)
+            choise = ''
+        elif choise == 'c':  # Coming to work
             conn.send('1'.encode())
-            datat=conn.recv(1024).decode()
+            datat = conn.recv(1024).decode()
             conn.send('1'.encode())
-            datast=bool(int(conn.recv(1024).decode()))
+            datast = bool(int(conn.recv(1024).decode()))
             conn.send('1'.encode())
             conn.send('1'.encode())
             datat_sec = int(conn.recv(1024).decode())
             conn.send('1'.encode())
-            print(datast,datat, datat_sec)
-            choise=''
-        elif choise=='a':
+            print(datast, datat, datat_sec)
+            choise = ''
+        elif choise == 'a':  # Admin Check
             conn.send('1'.encode())
             datatadm = conn.recv(1024).decode()
             conn.send('1'.encode())
             print(datatadm)
-        Con_num+=1
+        Con_num += 1
     except:
         pass
-
